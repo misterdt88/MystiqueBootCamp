e01a15b8599fc3bc959a673d6da2783f2c68f4a9
David Tran <misterdt88@gmail.com>
2020-03-10 00:27:17 -0400

    git practice

---
+++ js/vue.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        menuSection: 'Appetizers',
+        appetizers: []
+    },
+
+//    created() {
+//        fetch('appetizers.json')
+//            .then((response) => {
+//                return response.json();
+//            })
+//            .then((myJson) => {
+//                for (let i = 0; i < myJson.appetizers.length; i++) {
+//                    createAppetizerData(myJson.appetizers[i]);
+//                }
+//            });
+//    }
+});
